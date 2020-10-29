d500daee3d9525b00e654a884d574361ddc74e26
akankshashukla <dkgahlaut11@gmail.com>
2020-10-30 00:35:14 +0530

    add conditions for the wrong arguments

--- my_program.py
+++ my_program.py
@@ -20,7 +20,7 @@ class ParkingLot:
         self.occupiedSlots = 0
         self.slots = []
 
-    def create_parking_lot(self, total_slots):
+    def allot_parking_lot(self, total_slots):
         self.slots = [-1] * total_slots
         self.totalSlots = total_slots
         return self.totalSlots
@@ -30,7 +30,7 @@ class ParkingLot:
             if self.slots[i] == -1:
                 return i
 
-    def park(self, reg_number, color):
+    def park_car(self, reg_number, color):
         if self.occupiedSlots < self.totalSlots:
             slot_id = self.get_next_slot()
             self.slots[slot_id] = Car(reg_number, color)
@@ -40,7 +40,6 @@ class ParkingLot:
             return -1
 
     def leave(self, slot_id):
-
         if self.occupiedSlots > 0 and self.slots[slot_id - 1] != -1:
             self.slots[slot_id - 1] = -1
             self.occupiedSlots = self.occupiedSlots - 1
@@ -57,10 +56,8 @@ class ParkingLot:
                 continue
 
     def reg_no_by_color(self, color):
-
         list_reg_number = []
         for i in self.slots:
-
             if i == -1:
                 continue
             if i.color == color:
@@ -77,9 +74,7 @@ class ParkingLot:
         return -1
 
     def slot_no_by_color(self, color):
-
         list_slot_numbers = []
-
         for i in range(len(self.slots)):
             if self.slots[i] == -1:
                 continue
@@ -87,47 +82,69 @@ class ParkingLot:
                 list_slot_numbers.append(str(i + 1))
         return list_slot_numbers
 
-    def show(self, input_line):
+    def input_functions(self, input_line):
         if input_line.startswith('create_parking_lot'):
-            n = int(line.split(' ')[1])
-            res = self.create_parking_lot(n)
-            print('Created a parking lot with ' + str(res) + ' slots')
+            if len(line.split(' ')) == 2:
+                n = int(line.split(' ')[1])
+                res = self.allot_parking_lot(n)
+                message = 'Created a parking lot with ' + str(res) + ' slots'
+            else:
+                message = 'Please Provide the correct arguments'
+            print(message)
 
         elif input_line.startswith('park'):
-            reg_number = line.split(' ')[1]
-            color = line.split(' ')[2]
-            res = self.park(reg_number, color)
-            if res == -1:
-                print("Sorry, parking lot is full")
+            if len(line.split(' ')) == 3:
+                reg_number = line.split(' ')[1]
+                color = line.split(' ')[2]
+                res = self.park_car(reg_number, color)
+                if res == -1:
+                    message = "Sorry, parking lot is full"
+                else:
+                    message = 'Allocated slot number: ' + str(res)
             else:
-                print('Allocated slot number: ' + str(res))
+                message = "Please Provide car registration number and color"
+            print(message)
 
         elif input_line.startswith('leave'):
-            leave_slot_id = int(line.split(' ')[1])
-            status = self.leave(leave_slot_id)
-            if status:
-                print('Slot number ' + str(leave_slot_id) + ' is free')
+            if len(line.split(' ')) == 2:
+                leave_slot_id = int(line.split(' ')[1])
+                status = self.leave(leave_slot_id)
+                if status:
+                    message = 'Slot number ' + str(leave_slot_id) + ' is free'
+            else:
+                message = "Please provide the SlotId"
+            print(message)
 
         elif input_line.startswith('status'):
             self.status()
 
         elif input_line.startswith('registration_numbers_for_cars_with_colour'):
-            color = line.split(' ')[1]
-            reg_nos = self.reg_no_by_color(color)
-            print(', '.join(reg_nos))
+            if len(line.split(' ')) == 2:
+                color = line.split(' ')[1]
+                reg_nos = self.reg_no_by_color(color)
+                print(', '.join(reg_nos))
+            else:
+                print('Please Provide the Color')
 
         elif input_line.startswith('slot_numbers_for_cars_with_colour'):
-            color = line.split(' ')[1]
-            list_slot_number = self.slot_no_by_color(color)
-            print(', '.join(list_slot_number))
+            if len(line.split(' ')) == 2:
+                color = line.split(' ')[1]
+                list_slot_number = self.slot_no_by_color(color)
+                print(', '.join(list_slot_number))
+            else:
+                print('Please Provide the Color')
 
         elif input_line.startswith('slot_number_for_registration_number'):
-            reg_no = line.split(' ')[1]
-            slot_no = self.slot_no_by_reg_no(reg_no)
-            if slot_no == -1:
-                print("Not found")
+            if len(line.split(' ')) == 2:
+                reg_no = line.split(' ')[1]
+                slot_no = self.slot_no_by_reg_no(reg_no)
+                if slot_no == -1:
+                    print("Not found")
+                else:
+                    print(slot_no)
             else:
-                print(slot_no)
+                print('Please Provide the Registration Number')
+
         elif input_line.startswith('exit'):
             exit(0)
 
@@ -141,9 +158,9 @@ if __name__ == '__main__':
         with open(input_file) as f:
             for line in f:
                 line = line.rstrip('\n')
-                parking_lot.show(line)
+                parking_lot.input_functions(line)
     #Interactive mode
     else:
         while True:
             line = input("$ ")
-            parking_lot.show(line)
+            parking_lot.input_functions(line)
